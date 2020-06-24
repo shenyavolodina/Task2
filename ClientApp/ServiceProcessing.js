228a004df738496314a3cc93c371541b5ea79bc8
shenyavolodina <shenyavolodina@gmail.com>
2020-06-24 12:01:12 -0700

    initial commit

---
+++ ClientApp/ServiceProcessing.js
@@ -0,0 +1,86 @@
+﻿var params = { 'mark': 5 };
+
+function grater_than() {
+    inputData = document.getElementById("grater_input").value;
+    document.getElementById('grater_records_table').innerHTML = "<tbody><tr><th>Name</th><th>AvgMark</th></tr></tbody>";
+    if (inputData != '') {
+        params = { 'mark': inputData };
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:63414/StudentListWebService.asmx/GetStudentsGraterThan",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(params),
+            dataType: "json",
+            error: function (request, status, error) {
+
+                console.log("request:" + request);
+                console.log("status:" + status);
+                console.log("error:" + error);
+            }
+        }).done(function (response) {
+            var trHTML = '';
+            $.each(response.d, function (i, item) {
+                trHTML += '<tr><td>' + item.Name + '</td><td>' + item.AvgMark + '</td></tr>';
+            });
+            $('#grater_records_table').append(trHTML);
+        });
+    }
+    
+}
+
+function lower_than() {
+    inputData = document.getElementById("lower_input").value;
+    document.getElementById('lower_records_table').innerHTML = "<tbody><tr><th>Name</th><th>AvgMark</th></tr></tbody>";
+    if (inputData != '') {
+        params = { 'mark': inputData };
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:63414/StudentListWebService.asmx/GetStudentsLowerThan",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(params),
+            dataType: "json",
+            error: function (request, status, error) {
+
+                console.log("request:" + request);
+                console.log("status:" + status);
+                console.log("error:" + error);
+            }
+        }).done(function (response) {
+            var trHTML = '';
+            $.each(response.d, function (i, item) {
+                trHTML += '<tr><td>' + item.Name + '</td><td>' + item.AvgMark + '</td></tr>';
+            });
+            $('#lower_records_table').append(trHTML);
+        });
+    }
+
+}
+
+function in_range() {
+    inputDataMin = document.getElementById("range_input_min").value;
+    inputDataMax = document.getElementById("range_input_max").value;
+    document.getElementById('range_records_table').innerHTML = "<tbody><tr><th>Name</th><th>AvgMark</th></tr></tbody>";
+    if (inputDataMin != '' && inputDataMax != '') {
+        params = { 'minMark': inputDataMin, 'maxMark': inputDataMax};
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:63414/StudentListWebService.asmx/GetStudentsInRange",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(params),
+            dataType: "json",
+            error: function (request, status, error) {
+
+                console.log("request:" + request);
+                console.log("status:" + status);
+                console.log("error:" + error);
+            }
+        }).done(function (response) {
+            var trHTML = '';
+            $.each(response.d, function (i, item) {
+                trHTML += '<tr><td>' + item.Name + '</td><td>' + item.AvgMark + '</td></tr>';
+            });
+            $('#range_records_table').append(trHTML);
+        });
+    }
+
+}
---
+++ Task2/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace Task2.Models
+{
+    [Serializable]
+    public class Student
+    {
+        public string Name { get; set; }
+        public float AvgMark { get; set; }
+
+        public Student() 
+        {
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "*");
+                HttpContext.Current.Response.End();
+            }
+        }
+
+        public Student(string name, float avgMark)
+        {
+            Name = name;
+            AvgMark = avgMark;
+        }
+    }
+}
